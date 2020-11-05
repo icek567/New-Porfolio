33a97f470733153f1b736789cf2783e43ae88de1
Sergio Perez <perezsergio97.sp@gmail.com>
2020-11-04 19:31:47 -0600

    Dynamic changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-$('.collapse').collapse()
+
+
+!(function($) {
+    $(document).on('click', '.mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
+    });
+  
+    $(document).click(function(e) {
+      var container = $(".mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
+        }
+      }
+    });
+
+  
+})(jQuery);
