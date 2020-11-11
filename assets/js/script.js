9655e01d672a0c2d6ffc5d9ac18893495f4b1e20
Sergio Perez <perezsergio97.sp@gmail.com>
2020-11-10 19:22:53 -0600

    new dynamic features added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,28 @@
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
       $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times-circle');
-    });
-  
-    $(document).click(function(e) {
-      var container = $(".mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times-circle');
-        }
-      }
-    });
+  });
 
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times-circle');
+      }
+    }
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
   
 })(jQuery);
